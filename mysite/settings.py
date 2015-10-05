64d72d2d90e369004e7a2e1eb60b0fca2b5e784d
Dai Yu <lfeyao@gmail.com>
2015-10-05 09:47:43 -0700

    try serving static directly

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,11 +101,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'bucketlist/static'
 STATIC_URL = '/static/'
-
+"""
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '..', 'bucketlist', 'static'),
 )
+"""
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 LOGIN_URL = '/login/'
