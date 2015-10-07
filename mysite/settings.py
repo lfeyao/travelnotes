b182a89589b2344fa59daff78c4bc8de44efcfa5
Dai Yu <lfeyao@gmail.com>
2015-10-07 07:55:25 -0700

    all edits

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6=5dk327u)pub19%)*is_=&9lf=_6cb#txf481ytf-_fw*&t^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
